9f3b729a770a7cc7045f0cf09f34ba4196ff50e7
DeiMos13D <galstyan.david1993@gmail.com>
2018-06-21 16:13:28 +0400

    clean

--- src/AC/creditCardFocused.js
+++
@@ -1,8 +0,0 @@
-import { FOCUSED_CARD_FIELD } from '../constants'
-
-export function getFocusedCardField(focusedField) {
-    return {
-        type: FOCUSED_CARD_FIELD,
-        payload: focusedField
-    }
-}
